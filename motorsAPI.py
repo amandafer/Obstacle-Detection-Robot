b978edc8709027bb2bb752984c74ae5386119554
Amanda Fernandes <amandafer6@gmail.com>
2016-10-19 02:55:36 -0200

    Testing ET of motors controller

--- motorsAPI.py
+++ motorsAPI.py
@@ -1,30 +1,58 @@
 import explorerhat
 import time
+import timeit
+import sys
 
-def turnLeft(speed) {
+def turnLeft(speed):
 	explorerhat.motor.one.backwards(speed)
 	explorerhat.motor.two.forwards(speed)
-	sleep.time(0.5)
-	explorerhat.motor.stop()
-}
+	stopMotors()
 
-def turnRight(speed) {
+
+def turnRight(speed):
 	explorerhat.motor.one.forwards(speed)
 	explorerhat.motor.two.backwards(speed)
-	sleep.time(0.5)
-	explorerhat.motor.stop()
-}
+	stopMotors()
 
-def accelerate(speed) {
+
+def accelerate(speed):
 	explorerhat.motor.one.forwards(speed)
 	explorerhat.motor.two.forwards(speed)
-	sleep.time(0.5)
-	explorerhat.motor.stop()
-}
+	stopMotors()
 
-def reverse(speed) {
+
+def reverse(speed):
 	explorerhat.motor.one.backwards(speed)
         explorerhat.motor.two.backwards(speed)
-        sleep.time(0.5)
+	stopMotors()	
+
+
+
+def stopMotors():
+	time.sleep(0.5)
         explorerhat.motor.stop()
-}
+
+
+#Begining of ET testing
+def main():
+	direction = sys.argv[1]
+	speed = int(sys.argv[2])
+
+	if (direction == "left"):
+		turnLeft(speed)
+	elif (direction == "right"):
+                turnRight(speed)
+	elif (direction == "up"):
+                accelerate(speed)
+	elif (direction == "down"):
+              	reverse(speed)
+	else:
+		print("Wrong code")
+
+
+if __name__ == "__main__":
+	start_time = timeit.default_timer()
+	main()
+	elapsed = timeit.default_timer() - start_time
+
+	print (elapsed)
