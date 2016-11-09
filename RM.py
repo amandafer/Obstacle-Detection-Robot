af8ccd31896d8b3ba4a33dd4dcb34896640c80a6
Amanda Fernandes <amandafer6@gmail.com>
2016-11-09 01:34:35 -0200

    RM

--- RM.py
+++ RM.py
@@ -14,43 +14,6 @@ def _lcm(a,b): return abs(a * b) / fractions.gcd(a,b) if a and b else 0
 def lcm(a):
     return reduce(_lcm, a)
 
-#A task instance
-class TaskIns(object):
-     #Constructor (should only be invoked with keyword parameters)
-    def __init__(self, start, end, priority, name):
-        self.start    = start
-        self.end      = end
-        self.usage    = 0
-        self.priority = priority
-        self.name     = name.replace("\n", "")
-        self.id = int(random.random() * 10000)
-
-    #Allow an instance to use the cpu (periodic)
-    def use(self, usage):
-        self.usage += usage
-        if self.usage >= self.end - self.start:
-            return True
-        return False
-
-    #Default representation
-    def __repr__(self):
-        return str(self.name) + "#" + str(self.id) + " - start: " + str(self.start) + " priority: " + str(self.priority) + budget_text
-
-    #Get name as Name#id
-    def get_unique_name(self):
-        return str(self.name) + "#" + str(self.id)
-
-
-#Task types (templates for periodic tasks)
-class TaskType(object):
-    #Constructor
-    def __init__(self, period, release, execution, deadline, name):
-        self.period    = period
-        self.release   = release
-        self.execution = execution
-        self.deadline  = deadline
-        self.name      = name
-
 
 #Priority comparison
 def priority_cmp(one, other):
